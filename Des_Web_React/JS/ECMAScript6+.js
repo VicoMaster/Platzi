e034402d4df10ad32a914271d6ab8733490b2640
Andres Rivera <job.devcode@gmail.com>
2021-07-16 03:05:29 -0500

    Push automatico Fri Jul 16 03:05:29 2021

--- Des_Web_React/JS/ECMAScript6+.js
+++ Des_Web_React/JS/ECMAScript6+.js
@@ -1,1 +1,17 @@
 //Multilinea con comillas francesas
+let lorem2 = `otra frase epica
+ahora es otra frase epica`;
+
+//Desestructuración de elementos
+let person = {
+    'name': 'oscar',
+    'age': '23',
+    'country': 'mx'
+}
+let {name, age} = person;
+console.log(name, age);
+
+//Operador de propagación (spread): nos permite expandir de ciertas situaciones varios elementos
+let team1 = ['Malcon', 'Ximena', 'Rodolfo'];
+let team2 = ['Salome', 'Doris', 'Fabriccio'];
+let educaction = ['David', ...team1, ...team2];

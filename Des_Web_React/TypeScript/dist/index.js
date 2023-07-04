06e92fb32dac58a75db6986be6495f4cf86443b5
VicoMaster <job.devcode@gmail.com>
2023-07-04 04:05:47 -0500

    first compile tscript

---
+++ Des_Web_React/TypeScript/dist/index.js
@@ -0,0 +1,5 @@
+function retornaCosaLoca(params) {
+    const COSA_LOCA = `Esto es una cosa loca: ${params}`;
+    return params;
+}
+console.log(retornaCosaLoca('Andrés'));
---
+++ Des_Web_React/TypeScript/src/index.ts
@@ -0,0 +1,6 @@
+function retornaCosaLoca(params:string) {
+    const COSA_LOCA = `Esto es una cosa loca: ${params}`;
+    return params;
+}
+
+console.log(retornaCosaLoca('Andrés'));

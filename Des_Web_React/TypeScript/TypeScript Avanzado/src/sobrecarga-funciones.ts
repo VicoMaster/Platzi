e48c404a5e1442107f16b4cf03446d2cfd45a983
VicoMaster <job.devcode@gmail.com>
2023-11-18 21:41:51 -0500

    Push automatico Sat Nov 18 21:41:46 2023

--- Des_Web_React/TypeScript/TypeScript Avanzado/src/sobrecarga-funciones.ts
+++ Des_Web_React/TypeScript/TypeScript Avanzado/src/sobrecarga-funciones.ts
@@ -12,3 +12,23 @@ const rtaFunction = unirOarray('testArray');
 if (Array.isArray(rtaFunction)) {
     rtaFunction.reverse()
 }
+
+// Si quisiera manejar los metodos correspondientes a lo que devuelve la función se exporta de la siguiente manera
+
+
+export function unirOarrayDos(input: string | string): string[];
+export function unirOarrayDos(input: string | string[]): string;
+
+export function unirOarrayDos(input: string | string[]): string | string[] {
+    if (Array.isArray(input)) {
+        return input.join('');
+    } else {
+        return input.split('');
+    }
+}
+
+const STRING_MAYUS = unirOarrayDos(['Hola', 'Soy', 'Un', 'Texto', 'de', 'Arrays'])
+const ARRAY_TAMANNO = unirOarrayDos(['Soy String']);
+
+console.log('Quiero un String en Mayuscula : ' + STRING_MAYUS.toUpperCase());
+console.log('Quiero un Array: ' + ARRAY_TAMANNO.length);
